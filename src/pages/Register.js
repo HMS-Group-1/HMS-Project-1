3f29b68e29f38fd1baa8590b8dde271e6ce12ea1
Daniel Sharon <daanielsharon@gmail.com>
2022-07-09 15:26:14 +0700

    Adding seperate navigation login feature

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -26,7 +26,7 @@ const Register = () => {
 				no_telp: no_telp,
 				role: role,
 			});
-			navigateTo('/'); // diisi nanti
+			navigateTo('/');
 		} catch (error) {
 			if (error.response) {
 				console.log(error.response);
@@ -55,13 +55,13 @@ const Register = () => {
 	};
 
 	return (
-		<div className=" desktop:flex desktop:items-center desktop:h-screen desktop:justify-center pt-7">
-			<div className="desktop:w-4/12 laptop: w-ful mobile:w-full tablet:w-full ">
+		<div className=" pt-7 desktop:flex desktop:items-center desktop:h-screen desktop:justify-center tablet:mx-3">
+			<div className="desktop:w-4/12 laptop: w-ful  tablet:w-full mobile:w-full">
 				<div>
 					<img className=" desktop:w-12/12 w-9/12 tablet:mx-12 tablet:my-2 mobile:mx-12 " src={logo} alt="logo" />
 				</div>
 				<figure className="tablet:mx-12 mobile:mx-12">
-					<blockquote className="font-poppins font-semibold  text-justify desktop:py-7 desktop:text-2xl desktop:leading-loose  tablet:text-2xl tablet:my-2 tablet:py-5 tablet:leading-loose tablet:w-full mobile: pt-3 mobile:text-base">
+					<blockquote className="font-poppins font-semibold  text-justify desktop:py-7 desktop:text-2xl desktop:leading-loose  tablet:text-2xl tablet:my-2 tablet:py-5 tablet:leading-loose tablet:w-full mobile:my-5 mobile:text-base mobile:leading-loose">
 						Buku adalah pembawa peradaban. Tanpa buku, sejarah itu sunyi, sastra itu bodoh, sains lumpuh, pemikiran dan spekulasi terhenti. Buku adalah mesin perubahan, jendela, di dunia, mercusuar yang didirikan di lautan waktu
 					</blockquote>
 					<figcaption className="font-poppins font-semibold desktop:text-2xl tablet:text-xl tablet:pb-5 mobile:text-base"> -Barbara W.Tuchman</figcaption>
@@ -69,11 +69,11 @@ const Register = () => {
 			</div>
 
 			<div className="bg-red-50 flex justify-start flex-col rounded-md font-poppins  desktop:w-4/12 mobile:mx-12 mobile:my-5 mobile:px-5 mobile:items-center mobile:pb-8  ">
-				<h3 className=" font-semibold w-11/12 tablet:w-full tablet:pl-3 tablet:text-2xl mobile:pt-8  mobile:text-xl  mobile:w-11/12">Selamat Datang,</h3>
-				<h5 className="w-11/12 pt-0 pb-3 tablet:w-full tablet:pl-3 tablet:pt-1 tablet:text-base mobile:w-11/12 mobile:text-sm">Daftar akunmu dulu ya!</h5>
-				<form onSubmit={register} className="flex flex-col w-full mobile:px-3">
-					<p className="text-center text-merahTua font-bold text-lg">{message}</p>
-					<div className="desktop:flex desktop:flex-row desktop:items-center desktop:-mb-2">
+				<h3 className=" font-semibold w-11/12 tablet:w-full tablet:pl-4 tablet: tablet:text-2xl mobile:pt-8  mobile:text-xl  mobile:full ">Selamat Datang,</h3>
+				<h5 className="w-11/12 pt-0 pb-5 tablet:w-full tablet:pl-4 tablet:pt-1 tablet:text-base mobile:w-11/12 mobile:text-sm">Daftar akunmu dulu ya!</h5>
+				<form onSubmit={register} className="flex flex-col w-full mobile:px-4">
+					<p className="text-center text-merahTua font-bold text-lg -mb-2">{message}</p>
+					<div className="desktop:flex desktop:flex-row desktop:items-center desktop:-mb-2 tablet:mt-6 mobile:mt-6">
 						<div className="desktop:w-full">
 							<label className="pb-2" for="nama">
 								Nama
@@ -88,14 +88,14 @@ const Register = () => {
 							/>
 							<p className="text-center text-merahTua font-bold text-base desktop:hidden">{namaErrorMessage}</p>
 						</div>
-						<div className="desktop:mt-2 desktop:w-full">
+						<div className="desktop:mt-2 desktop:w-full tablet:mt-2 mobile:mt-2">
 							<label className="pb-2" for="no_telp">
 								Nomor Telepon
 							</label>
 							<input
 								value={no_telp}
 								onChange={(e) => setNo_telp(e.target.value)}
-								class=" appearance-none border rounded w-full py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								class=" appearance-none border rounded w-full py-2 px-3 mb-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 								id="no_telp"
 								type="text"
 								oninput={(e) => e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '1')}
@@ -108,7 +108,7 @@ const Register = () => {
 						<p className="text-center text-merahTua font-bold text-lg desktop:text-sm  desktop:w-full">{namaErrorMessage} </p>
 						<p className="text-center text-merahTua font-bold text-lg desktop:w-full  desktop:text-sm">{no_telpErrorMessage}</p>
 					</div>
-					<div className="desktop:flex desktop:flex-row desktop:items-center">
+					<div className="desktop:flex desktop:flex-row desktop:items-center ">
 						<div className="desktop:w-full">
 							<label className="pb-2 desktop:mr-4" for="Email">
 								Email
@@ -123,14 +123,14 @@ const Register = () => {
 							/>
 						</div>
 						<p className="text-center text-merahTua font-bold text-base  desktop:hidden desktop:text-sm desktop:w-full">{emailErrorMessage} </p>
-						<div className="desktop:w-full">
+						<div className="desktop:w-full tablet:mt-2 mobile:mt-2">
 							<label className="pb-2" for="password">
 								Password
 							</label>
 							<input
 								value={password}
 								onChange={(e) => setPassword(e.target.value)}
-								class="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline "
+								class="appearance-none border rounded w-full py-2 px-3 mb-5 text-gray-700  leading-tight focus:outline-none focus:shadow-outline "
 								id="password"
 								type="password"
 								placeholder="******************"
@@ -143,7 +143,7 @@ const Register = () => {
 						<p className="text-center text-merahTua font-bold text-lg desktop: w-full desktop:text-sm">{passwordErrorMessage}</p>
 					</div>
 
-					<label className="pb-2" for="role">
+					<label className="pb-2 tablet:mt-2 mobile:mt-2" for="role">
 						Role
 					</label>
 					<select className="bg-white border rounded w-full py-2 px-3 mb-4" id="role">
@@ -156,7 +156,11 @@ const Register = () => {
 							daftar
 						</button>
 						<p class="inline-block align-baseline font-bold text-sm text-blue-500 tablet:pt-2 ">
-							Sudah punya akun? <Link to={'/'}> Masuk di sini</Link>{' '}
+							Sudah punya akun?{' '}
+							<Link className="hover:underline" to={'/'}>
+								{' '}
+								Masuk di sini
+							</Link>{' '}
 						</p>
 					</div>
 				</form>
