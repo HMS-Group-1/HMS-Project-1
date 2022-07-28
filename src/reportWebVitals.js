a42973a46e032a8b54d977d0a6630bce6aa43cdd
Daniel Sharon <daanielsharon@gmail.com>
2022-07-28 09:32:34 +0700

    Resolving Conflict

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
@@ -12,18 +11,3 @@ const reportWebVitals = onPerfEntry => {
 };
 
 export default reportWebVitals;
-=======
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
-
-export default reportWebVitals;
->>>>>>> bf61aabcddd8a645ab36c3c2b2c4c9124f2b0de5
