e72b2fd318e782d7db1b6ef1e1bd878bb316c4aa
Daniel Sharon <daanielsharon@gmail.com>
2022-07-28 09:45:02 +0700

    Resolving Conflict

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
-<<<<<<< HEAD
 import './App.css';
 import Footer from './components/Footer';
 import Router from './router';
-import { ContextWrapper } from './helpers/context'
+import { ContextWrapper } from './helpers/context';
 
 function App() {
-  return (
-    <ContextWrapper>
-        <div className="App h-full">
-          <Router />
-          <br />
-          <br />
-          <Footer />
-        </div>
-    </ContextWrapper>
-  );
+	return (
+		<ContextWrapper>
+			<div className="App h-full">
+				<Router />
+				<br />
+				<br />
+				<Footer />
+			</div>
+		</ContextWrapper>
+	);
 }
 
 export default App;
-=======
-import './App.css';
-import Footer from './components/Footer';
-import Router from './router';
-
-function App() {
-  return (
-    <div className="App">
-      <Router/>
-      <br/>
-      <br/>
-      <Footer/>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> bf61aabcddd8a645ab36c3c2b2c4c9124f2b0de5
