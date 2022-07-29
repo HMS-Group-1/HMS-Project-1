5341c33fb24463e09ac8cc47790e6fc3114bebfe
Daniel Sharon <daanielsharon@gmail.com>
2022-07-29 15:11:47 +0700

    New Dummy File

--- src/router.js
+++ src/router.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-
 import PinjamBuku from './pages/pinjambuku';
 import Register from './pages/Register';
 import Admintest from './test/Admintest';
+import GetBook from './test/booktest';
 
 function Router() {
 	return (
 		<Routes>
 			<Route path="/" element={<Login />} />
+			<Route path="/admin/book" element={<GetBook />} />
+			<Route path="/detail/:id" />
 			<Route path="/admin/user" element={<Admintest />} />
 			<Route path="/register" element={<Register />} />
 			<Route path="/pinjambuku" element={<PinjamBuku />} />
---
+++ src/test/booktest.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import axios from '../helpers/interceptor';
+import { Link } from 'react-router-dom';
+
+function toBase64(img) {
+	//arr = new Uint8Array(arr) if it's an ArrayBuffer
+	return btoa(img.reduce((data, byte) => data + String.fromCharCode(byte), ''));
+}
+
+const GetBook = () => {
+	const [response, setResponse] = useState([]);
+
+	console.log(response);
+
+	useEffect(() => {
+		fetchBook();
+	}, []);
+
+	const fetchBook = async () => {
+		const response = await axios.get('http://localhost:5000/admin/book');
+		setResponse(response.data);
+	};
+
+	return (
+		<div>
+			{response.length > 0
+				? response.map((book) => (
+						<Link to={`detail/${book.id}`}>
+							<div>Nama Buku: {book.judul_buku}</div>
+							<div>Kategori Buku: {book.kategori_id}</div>
+							<div>Deskripsi Buku: {book.deskripsi}</div>
+							<img src={toBase64(book.gambar.data)} alt={book.judul_buku} />
+							<div>Rak: {book.rak_id}</div>
+							<div>Tahun terbit: {book.tahun_terbit}</div>
+						</Link>
+				  ))
+				: 'loading...'}
+		</div>
+	);
+};
+
+export default GetBook;
