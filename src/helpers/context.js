df874c463ab9e0f664a5457554ad0a6189edea3a
Daniel Sharon <daanielsharon@gmail.com>
2022-08-01 11:25:23 +0700

    Securing localStorage

--- src/helpers/context.js
+++ src/helpers/context.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 export const ContextProvider = React.createContext(null);
 
 export const ContextWrapper = (props) => {
-	const [isLogin, setIsLogin] = useState({
+	const [isLogin, setIsLogin] = useLocalStorage('isLogin', {
 		nama: '',
 		id: 0,
 		status: false,
