51e1c06e5e6a5df48eed05bf930006fe4cf7b06f
Daniel Sharon <daanielsharon@gmail.com>
2022-08-10 10:29:01 +0700

    Improving logout navbar

--- src/pages/Admin/BookList.js
+++ src/pages/Admin/BookList.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useContext} from "react";
-import axios from "../../helpers/interceptor";
+import React, { useState, useEffect, useContext } from 'react';
+import axios from '../../helpers/interceptor';
+import notInterceptor from 'axios';
 import Logo from '../../assets/capture1.PNG';
 import { Link, useNavigate } from 'react-router-dom';
 import { ContextProvider } from '../../helpers/context';
@@ -28,26 +29,26 @@ const GetBook = () => {
 
 	const deleteBook = async (id) => {
 		try {
-		  await axios.delete(`http://localhost:5000/admin/deleteBook/${id}`);
-		  fetchBook();
+			await notInterceptor.delete(`http://localhost:5000/admin/deleteBook/${id}`);
+			fetchBook();
 		} catch (error) {
-		  console.log(error);
+			console.log(error);
 		}
-	  };
+	};
 
-	  const truncateString = (str, num) => {
+	const truncateString = (str, num) => {
 		if (str?.length > num) {
-			return str.slice(0, num) + '...'
+			return str.slice(0, num) + '...';
 		} else {
-			return str
+			return str;
 		}
 	};
 
 	const logOutHandler = async () => {
 		try {
-			await axios.delete('http://localhost:5000/logout');
-			localStorage.removeItem('isLogin');
+			await notInterceptor.delete('http://localhost:5000/logout');
 			setIsLogin({ nama: '', id: null, status: false });
+			localStorage.removeItem('isLogin');
 			navigateTo('/');
 		} catch (error) {
 			console.log(error);
@@ -57,76 +58,85 @@ const GetBook = () => {
 	const menuHandler = () => {
 		setMenu(!menu);
 	};
-	
+
 	function getInitials(string) {
 		var names = string.split(' '),
 			initials = names[0].substring(0, 1).toUpperCase();
-		
+
 		if (names.length > 1) {
 			initials += names[names.length - 1].substring(0, 1).toUpperCase();
 		}
 		return initials;
-	};
-
+	}
 
 	return (
 		<>
-		<div className="flex w-full items-center p-5">
-			<div className="w-[80px] h-[80px] px-3 py-2 bg-yellow-300 rounded-full">
-        	<p onClick={menuHandler} className="w-full h-full relative hover:cursor-pointer flex justify-center items-center text-lg text-white font-medium">
-    		{getInitials(isLogin.nama)}
-        	</p>
-			{menu && (
-								<div className="bg-blue-400 z-20 mt-1 rounded-md w-[180px] py-2 absolute left-3 top-11">
-									<ul className="flex flex-col gap-2">
-										<Link to="/admin/user">
-											<li className="text-sm hover:bg-blue-500 py-1 text-white pl-2">User List</li>
-										</Link>
-										<Link to="/">
-											<li className="text-sm hover:bg-blue-500 py-1 text-white pl-2" onClick={logOutHandler}>
-												Keluar
-											</li>
-										</Link>
-									</ul>
-								</div>
-							)}
-      </div>
-	  <Link to="/">
-			<button onClick={logOutHandler} className="bg-merahTua text-white px-3 py-1 ml-3 rounded-md">
-				Logout
-			</button>
-		</Link>
-	  </div>
-		<div>
-		
-		</div>
+			<div className="flex w-full items-center p-5">
+				<div className="w-[80px] h-[80px] px-3 py-2 bg-yellow-300 rounded-full">
+					<p onClick={menuHandler} className="w-full h-full relative hover:cursor-pointer flex justify-center items-center text-lg text-white font-medium">
+						{getInitials(isLogin.nama)}
+					</p>
+					{menu && (
+						<div className="bg-blue-400 z-20 mt-1 rounded-md w-[180px] py-2 absolute left-3 top-11">
+							<ul className="flex flex-col gap-2">
+								<Link to="/admin/user">
+									<li className="text-sm hover:bg-blue-500 py-1 text-white pl-2">User List</li>
+								</Link>
+								<Link to="/">
+									<li className="text-sm hover:bg-blue-500 py-1 text-white pl-2" onClick={logOutHandler}>
+										Keluar
+									</li>
+								</Link>
+							</ul>
+						</div>
+					)}
+				</div>
+				<Link to="/">
+					<button onClick={logOutHandler} className="bg-merahTua text-white px-3 py-1 ml-3 rounded-md">
+						Logout
+					</button>
+				</Link>
+			</div>
+			<div></div>
 			<div className=" p-5 font-sans">
 				<img src={Logo} alt="logo" className="mb-5 w-[300px]" />
-				<h1 className='text-center'>Daftar Buku</h1>
-				<br /><br /><br/>
-				
-				<Link to={`/admin/createBook`} className="bg-biru text-white px-3 py-1 rounded-md">Create new</Link>
-				<br /><br />
+				<h1 className="text-center">Daftar Buku</h1>
+				<br />
+				<br />
+				<br />
+
+				<Link to={`/admin/createBook`} className="bg-biru text-white px-3 py-1 rounded-md">
+					Create new
+				</Link>
+				<br />
+				<br />
 				{response.length > 0
 					? response.map((book) => (
-						<div className="border-solid border-2 border-black p-3 ">
-							<div><img src={`data:image/png;base64, ${toBase64(book.gambar.data)}`} alt={book.judul_buku} className="h-[200px] w-[200px]" /></div>
-							<div className="pt-3 font-bold">Nama Buku: {book.judul_buku}</div>
-							<div className="pt-3">Kategori Buku: {book.kategori_id} </div>
-							<div className="pt-3">Deskripsi Buku: {truncateString(book.deskripsi, 1000)} </div>
-							<div><div className="pt-3">Rak: {book.rak_id}</div></div>
-							<div><div className="pt-3">Tahun terbit: {book.tahun_terbit}</div></div>
-							<Link to={`/admin/updateBook/${book.id}`} className="bg-biru text-white px-3 py-2 rounded-md">Edit</Link>
-							<span className='pl-3'></span>
-							<button onClick={() => deleteBook(book.id)}
-								className="bg-merahTua text-white px-3 py-2 rounded-md">
-								Delete</button>
-						</div>
-
-					))
+							<div className="border-solid border-2 border-black p-3 ">
+								<div>
+									<img src={`data:image/png;base64, ${toBase64(book.gambar.data)}`} alt={book.judul_buku} className="h-[200px] w-[200px]" />
+								</div>
+								<div className="pt-3 font-bold">Nama Buku: {book.judul_buku}</div>
+								<div className="pt-3">Kategori Buku: {book.kategori_id} </div>
+								<div className="pt-3">Deskripsi Buku: {truncateString(book.deskripsi, 1000)} </div>
+								<div>
+									<div className="pt-3">Rak: {book.rak_id}</div>
+								</div>
+								<div>
+									<div className="pt-3">Tahun terbit: {book.tahun_terbit}</div>
+								</div>
+								<Link to={`/admin/updateBook/${book.id}`} className="bg-biru text-white px-3 py-2 rounded-md">
+									Edit
+								</Link>
+								<span className="pl-3"></span>
+								<button onClick={() => deleteBook(book.id)} className="bg-merahTua text-white px-3 py-2 rounded-md">
+									Delete
+								</button>
+							</div>
+					  ))
 					: 'loading...'}
-			</div></>
-		
+			</div>
+		</>
 	);
 };
 
--- src/pages/Admin/UserList.js
+++ src/pages/Admin/UserList.js
@@ -1,132 +1,128 @@
-import React, { useState, useEffect, useContext} from "react";
-import axios from "../../helpers/interceptor";
+import React, { useState, useEffect, useContext } from 'react';
+import axios from '../../helpers/interceptor';
+import notInterceptor from 'axios';
 import Logo from '../../assets/capture1.PNG';
 import { Link, useNavigate } from 'react-router-dom';
 import { ContextProvider } from '../../helpers/context';
 import { getInitials } from '../../helpers/constant';
 
 const UserList = () => {
-  const [users, setUser] = useState([]);
-  const { setIsLogin } = useContext(ContextProvider);
+	const [users, setUser] = useState([]);
+	const { setIsLogin } = useContext(ContextProvider);
 	const navigateTo = useNavigate();
 	const { isLogin } = useContext(ContextProvider);
 	const [menu, setMenu] = useState(false);
-  useEffect(() => {
-    getUsers();
-  }, []);
+	useEffect(() => {
+		getUsers();
+	}, []);
 
+	const getUsers = async () => {
+		const response = await axios.get('http://localhost:5000/admin/user/');
+		setUser(response.data);
+	};
 
-  const getUsers = async () => {
-    const response = await axios.get("http://localhost:5000/admin/user/");
-    setUser(response.data);
-  };
-
-  const deleteUser = async (id) => {
-    try {
-      await axios.delete(`http://localhost:5000/admin/deleteUser/${id}`);
-      getUsers();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const menuHandler = () => {
+	const deleteUser = async (id) => {
+		try {
+			await axios.delete(`http://localhost:5000/admin/deleteUser/${id}`);
+			getUsers();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	const menuHandler = () => {
 		setMenu(!menu);
 	};
 
 	const logOutHandler = async () => {
 		try {
-			await axios.delete('http://localhost:5000/logout');
-			localStorage.removeItem('isLogin');
+			await notInterceptor.delete('http://localhost:5000/logout');
 			setIsLogin({ nama: '', id: null, status: false });
+			localStorage.removeItem('isLogin');
 			navigateTo('/');
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
-  return (
-
-    <div className="desktop:sticky desktop:top-0 desktop:z-20 desktop:pb-1 bg-white rounded-b-lg p-5">
-    <div className="flex gap-2 ">
-      <Link to="/">
-        <button onClick={logOutHandler} className="bg-merahTua text-white px-3 py-2 rounded-md flex">
-          Logout
-        </button>
-      </Link>
-      <div className="w-100 h-100 px-3 py-2 bg-yellow-300 rounded-full">
-        <p onClick={menuHandler} className="w-full h-full relative hover:cursor-pointer flex justify-center items-center text-lg text-white font-medium">
-          {getInitials(isLogin.nama)}
-        </p>
-        {menu && (
-							<div className="bg-blue-400 z-20 mt-1 rounded-md w-[180px] py-2 absolute left-3 top-11">
-								<ul className="flex flex-col gap-2">
-									<Link to="/admin/book">
-										<li className="text-sm hover:bg-blue-500 py-1 text-white pl-2">List Buku</li>
-									</Link>
-									<Link to="/">
-										<li className="text-sm hover:bg-blue-500 py-1 text-white pl-2" onClick={logOutHandler}>
-											Keluar
-										</li>
-									</Link>
-								</ul>
-							</div>
-						)}
-      </div>
-    </div><br /><div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-        <div>
-          <img src={Logo} alt="logo" className="w-[300px]" />
-          <br /><br />
-          <h1 className="title uppercase">Daftar Admin dan anggota</h1>
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className='text-xs text-gray-700 uppercase bg-blue-300 dark:bg-gray-700 dark:text-gray-400'>
-              <tr>
-                <th scope="col" className="py-3 px-6">
-                  No
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Nama
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Email
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  No Telpon
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Role
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user, index) => (
-                <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700" key={user.id}>
-                  <td className="py-4 px-6">{index + 1}</td>
-                  <td className="py-4 px-6">{user.nama}</td>
-                  <td className="py-4 px-6">{user.email}</td>
-                  <td className="py-4 px-6">{user.no_telp}</td>
-                  <td className="py-4 px-6">{user.role}</td>
-                  <td>
-                    <Link
-                      to={`edit/${user.id}`}
-                      className="bg-biru text-white px-3 py-1 rounded-md"
-                    >
-                      Edit
-                    </Link>
-                    <button
-                      onClick={() => deleteUser(user.id)}
-                      className="bg-merahTua text-white px-2 py-1 rounded-md"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-</div>
-  );
+	return (
+		<div className="desktop:sticky desktop:top-0 desktop:z-20 desktop:pb-1 bg-white rounded-b-lg p-5">
+			<div className="flex gap-2 ">
+				<Link to="/">
+					<button onClick={logOutHandler} className="bg-merahTua text-white px-3 py-2 rounded-md flex">
+						Logout
+					</button>
+				</Link>
+				<div className="w-100 h-100 px-3 py-2 bg-yellow-300 rounded-full">
+					<p onClick={menuHandler} className="w-full h-full relative hover:cursor-pointer flex justify-center items-center text-lg text-white font-medium">
+						{getInitials(isLogin.nama)}
+					</p>
+					{menu && (
+						<div className="bg-blue-400 z-20 mt-1 rounded-md w-[180px] py-2 absolute left-3 top-11">
+							<ul className="flex flex-col gap-2">
+								<Link to="/admin/book">
+									<li className="text-sm hover:bg-blue-500 py-1 text-white pl-2">List Buku</li>
+								</Link>
+								<Link to="/">
+									<li className="text-sm hover:bg-blue-500 py-1 text-white pl-2" onClick={logOutHandler}>
+										Keluar
+									</li>
+								</Link>
+							</ul>
+						</div>
+					)}
+				</div>
+			</div>
+			<br />
+			<div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+				<div>
+					<img src={Logo} alt="logo" className="w-[300px]" />
+					<br />
+					<br />
+					<h1 className="title uppercase">Daftar Admin dan anggota</h1>
+					<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+						<thead className="text-xs text-gray-700 uppercase bg-blue-300 dark:bg-gray-700 dark:text-gray-400">
+							<tr>
+								<th scope="col" className="py-3 px-6">
+									No
+								</th>
+								<th scope="col" className="py-3 px-6">
+									Nama
+								</th>
+								<th scope="col" className="py-3 px-6">
+									Email
+								</th>
+								<th scope="col" className="py-3 px-6">
+									No Telpon
+								</th>
+								<th scope="col" className="py-3 px-6">
+									Role
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							{users.map((user, index) => (
+								<tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700" key={user.id}>
+									<td className="py-4 px-6">{index + 1}</td>
+									<td className="py-4 px-6">{user.nama}</td>
+									<td className="py-4 px-6">{user.email}</td>
+									<td className="py-4 px-6">{user.no_telp}</td>
+									<td className="py-4 px-6">{user.role}</td>
+									<td>
+										<Link to={`edit/${user.id}`} className="bg-biru text-white px-3 py-1 rounded-md">
+											Edit
+										</Link>
+										<button onClick={() => deleteUser(user.id)} className="bg-merahTua text-white px-2 py-1 rounded-md">
+											Delete
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default UserList;
