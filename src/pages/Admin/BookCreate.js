7699b45d0098916f581e5b47b984d76d2a4ccb48
Daniel Sharon <daanielsharon@gmail.com>
2022-08-11 11:35:06 +0700

    Adding error log for book uploading

--- src/pages/Admin/BookCreate.js
+++ src/pages/Admin/BookCreate.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from "../../helpers/interceptor";
+import axios from '../../helpers/interceptor';
 import Logo from '../../assets/capture1.PNG';
 import './Admintest.css';
 
@@ -14,6 +14,7 @@ const Bookcreate = () => {
 	const [deskripsiError, setDeskripsiError] = useState('');
 	const [preview, setPreview] = useState('');
 	const [fileToUpload, setFileToUpload] = useState('');
+	const [gambarError, setGambarError] = useState('');
 	const [stok, setStok] = useState('');
 	const [stokError, setStokError] = useState('');
 	const [rak_id, setRak_id] = useState([]);
@@ -66,12 +67,14 @@ const Bookcreate = () => {
 			navigateTo('/admin/book');
 		} catch (error) {
 			if (error.response.data.errors) {
+				console.log(error.response.data.errors);
 				setJudulError('');
 				setKategoriError('');
 				setDeskripsiError('');
 				setStokError('');
 				setRakError('');
 				setTahunError('');
+				setGambarError('');
 				error.response.data.errors.map((e) => {
 					if (e.param === 'judul_buku') setJudulError(e.msg);
 					if (e.param === 'kategori_id') setKategoriError(e.msg);
@@ -79,6 +82,7 @@ const Bookcreate = () => {
 					if (e.param === 'stok') setStokError(e.msg);
 					if (e.param === 'rak_id') setRakError(e.msg);
 					if (e.param === 'tahun_terbit') setTahunError(e.msg);
+					if (e.param === 'gambar') setGambarError(e.msg);
 				});
 			}
 		}
@@ -87,27 +91,15 @@ const Bookcreate = () => {
 	return (
 		<div>
 			<form className="container pl-4" onSubmit={submit}>
-			<img src={Logo} alt="logo" className='w-[300px] mt-5 pb-3'/>
-			<h1 className='text-center'>Create Book</h1>
-				<label className='form-label'>Judul Buku: </label>
-				<input  
-				type="text" 
-				className='form-control border-2 ' 
-				value={judul_buku} 
-				onChange={(e) => setjudul_buku(e.target.value)} />
+				<img src={Logo} alt="logo" className="w-[300px] mt-5 pb-3" />
+				<h1 className="text-center">Create Book</h1>
+				<label className="form-label">Judul Buku: </label>
+				<input type="text" className="form-control border-2 " value={judul_buku} onChange={(e) => setjudul_buku(e.target.value)} />
 				{judulError}
-				<label 
-				className="form-label ">
-					Kategori
-					</label>
+				<label className="form-label ">Kategori</label>
 				{kategori_id.length > 0 ? (
-					<select 
-					className='form-select border-2' onChange={(e) => 
-					setSelectedKategori(parseInt(e.target.value))}>
-						<option 
-						selected value="" 
-						disabled hidden>
-						</option>
+					<select className="form-select border-2" onChange={(e) => setSelectedKategori(parseInt(e.target.value))}>
+						<option selected value="" disabled hidden></option>
 						{kategori_id.map((kategori, index) => (
 							<option key={index} value={kategori.id}>
 								{index + 1}
@@ -115,45 +107,30 @@ const Bookcreate = () => {
 						))}
 					</select>
 				) : (
-					<select 
-					className='form-select border-2'>
-						<option>
-							Please Wait..
-							</option>
+					<select className="form-select border-2">
+						<option>Please Wait..</option>
 					</select>
 				)}
 				{kategoriError}
-				<label 
-				className="form-label">
-					Deskripsi
-					</label>
-				<textarea 
-				className='form-control border-2' 
-				rows="5" type="text" 
-				value={deskripsi} 
-				onChange={(e) => setDeskripsi(e.target.value)} />
+				<label className="form-label">Deskripsi</label>
+				<textarea className="form-control border-2" rows="5" type="text" value={deskripsi} onChange={(e) => setDeskripsi(e.target.value)} />
 				{deskripsiError}
-				<label 
-				className="form-label">
-					Masukkan Gambar
-					</label>
+				<label className="form-label">Masukkan Gambar</label>
 				{preview ? <img src={preview} alt="Upload gagal" /> : null}
-				<label 
-				className="file-upload ">
-					<input 
-					className="form-control border-2" 
-					type="file" 
-					onChange={changeImage} />
+				<label className="file-upload ">
+					<input
+						className="block w-full text-sm text-gray-900 bg-gray-50 rounded-sm border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+						type="file"
+						onChange={changeImage}
+					/>
 				</label>
-				<label 
-				className="form-label">
-					Stok
-					</label>
+				{gambarError}
+				<label className="form-label">Stok</label>
 				<input className="form-control border-2" type="text" value={stok} onChange={(e) => setStok(e.target.value)} />
 				{stokError}
 				<label className="form-label">Rak</label>
 				{rak_id.length > 0 ? (
-					<select className='form-select border-2' onChange={(e) => setSelectedRak(parseInt(e.target.value))}>
+					<select className="form-select border-2" onChange={(e) => setSelectedRak(parseInt(e.target.value))}>
 						<option selected value="" disabled hidden></option>
 						{rak_id.map((rak, index) => (
 							<option key={index} value={rak.id}>
@@ -168,9 +145,11 @@ const Bookcreate = () => {
 				)}
 				{rakError}
 				<label className="form-label ">Tahun Terbit</label>
-				<input className='form-control border-2' placeholder='FORMAT YYYY' type="text" value={tahun_terbit} onChange={(e) => setTahun_terbit(e.target.value)} />
+				<input className="form-control border-2" placeholder="FORMAT YYYY" type="text" value={tahun_terbit} onChange={(e) => setTahun_terbit(e.target.value)} />
 				{tahunError}
-				<button type="submit" class="bg-biru text-white px-3 py-2 rounded-md">Send</button>
+				<button type="submit" class="bg-biru text-white px-3 py-2 rounded-md">
+					Send
+				</button>
 			</form>
 		</div>
 	);
